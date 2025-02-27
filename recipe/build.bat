4745dbda1ccd097cac5f9a9f6abcd586f9dba72c
Olivier Roussel <olivier.roussel@inria.fr>
2025-02-27 14:28:01 +0100

    enable windows build

--- recipe/build.bat
+++ recipe/build.bat
@@ -9,7 +9,6 @@ FOR %%l in (^
     jpeg ^
     openal ^
     openssl ^
-    openexr ^
     png ^
     python ^
     tiff ^
@@ -31,12 +30,13 @@ FOR %%l in (^
 )
 
 :: Make panda using special panda3d tool
-%PYTHON makepanda/makepanda.py ^
+:: Use vs2019 compiler (msvc_version 14.2)
+%PYTHON% makepanda/makepanda.py ^
     --threads=2 ^
     --outputdir=build ^
     --wheel ^
     --everything ^
-    --msvc-version=14.1 ^
+    --msvc-version=14.2 ^
     --windows-sdk=10 ^
     %ADDITIONAL_OPTIONS
 if errorlevel 1 exit 1
@@ -44,3 +44,26 @@ if errorlevel 1 exit 1
 :: Install wheel which install python, bin
 %PYTHON% -m pip install panda3d*.whl -vv
 if errorlevel 1 exit 1
+
+cd build
+
+:: Install lib in sysroot-folder
+robocopy lib %LIBRARY_LIB% /E >nul
+
+:: Make etc 
+mkdir %LIBRARY_PREFIX%\etc\panda3d
+robocopy etc %LIBRARY_PREFIX%\etc\panda3d /E >nul
+
+:: Install headers
+mkdir %LIBRARY_INC%\panda3d
+robocopy include %LIBRARY_INC%\panda3d /E >nul
+
+:: Make share
+mkdir %LIBRARY_PREFIX%\share\panda3d\models
+robocopy models %LIBRARY_PREFIX%\share\panda3d\models /E >nul
+
+mkdir %LIBRARY_PREFIX%\share\panda3d\plugins
+robocopy plugins %LIBRARY_PREFIX%\share\panda3d\plugins /E >nul
+
+copy ReleaseNotes %LIBRARY_PREFIX%\share\panda3d\
+copy LICENSE %LIBRARY_PREFIX%\share\panda3d\
