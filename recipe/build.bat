9ca94d2825864d5b73adb5cd60eb06a222b5093a
Yann de Mont-Marin <yann.montmarin@gmail.com>
2025-03-03 21:30:03 +0100

    Update build.bat

--- recipe/build.bat
+++ recipe/build.bat
@@ -87,7 +87,7 @@ copy LICENSE %LIBRARY_PREFIX%\share\panda3d\
 :: This will allow them to be run on environment activation.
 for %%F in (activate deactivate) DO (
   if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
-  copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+  copy %RECIPE_DIR%\scripts\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
   :: Copy unix shell activation scripts, needed by Windows Bash users
-  copy %RECIPE_DIR%\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
+  copy %RECIPE_DIR%\scripts\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
 )
