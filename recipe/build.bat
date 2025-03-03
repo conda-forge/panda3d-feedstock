bd9ab49497256a80796055151a39a224b4ef9452
Olivier Roussel <olivier.roussel@inria.fr>
2025-03-03 14:06:15 +0100

    add activation scripts to set PRC environment variable

--- recipe/build.bat
+++ recipe/build.bat
@@ -65,3 +65,12 @@ robocopy plugins %LIBRARY_PREFIX%\share\panda3d\plugins /E >nul
 
 copy ReleaseNotes %LIBRARY_PREFIX%\share\panda3d\
 copy LICENSE %LIBRARY_PREFIX%\share\panda3d\
+
+:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
+:: This will allow them to be run on environment activation.
+for %%F in (activate deactivate) DO (
+  if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
+  copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+  :: Copy unix shell activation scripts, needed by Windows Bash users
+  copy %RECIPE_DIR%\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
+)
--- recipe/build.sh
+++ recipe/build.sh
@@ -75,3 +75,11 @@ rsync -a plugins              $PREFIX/share/panda3d
 rsync -a samples              $PREFIX/share/panda3d
 cp ReleaseNotes               $PREFIX/share/panda3d
 cp LICENSE                    $PREFIX/share/panda3d
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
