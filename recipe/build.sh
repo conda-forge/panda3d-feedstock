1b94e5277748e55dec44b35d7034ce811b163314
Yann de Mont-Marin <yann.montmarin@gmail.com>
2025-03-03 21:29:34 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -88,5 +88,5 @@ cp LICENSE                           $PREFIX/share/panda3d
 for CHANGE in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
 done
