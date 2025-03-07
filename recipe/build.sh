74b2918b0ce684bca898e7798ae9b0ed4fa2735a
Olivier Roussel <olivier.roussel@inria.fr>
2025-03-07 16:48:38 +0100

    try to debug incorrect host machine detected

--- recipe/build.sh
+++ recipe/build.sh
@@ -70,12 +70,12 @@ $PYTHON --version
 $PREFIX/bin/python --version
 $BUILD_PREFIX/bin/python --version
 
-$BUILD_PREFIX/bin/python -c "import sysconfig, pprint; pprint.pprint(sysconfig.get_config_vars())"
-$BUILD_PREFIX/bin/python -c "import platform;print(platform.machine())"
+$PREFIX/../build_env/bin/python -c "import sysconfig, pprint; pprint.pprint(sysconfig.get_config_vars())"
+$PREFIX/../build_env/bin/python -c "import platform;print(platform.machine())"
 echo "-------------------------------"
 
 # Build panda using special panda3d tool
-$PYTHON makepanda/makepanda.py \
+$PREFIX/../build_env/bin/python makepanda/makepanda.py \
     --wheel \
     --threads=${CPU_COUNT} \
     --outputdir=build \
@@ -84,7 +84,7 @@ $PYTHON makepanda/makepanda.py \
     $ADDITIONAL_OPTIONS
 
 # Install wheel which install python site-package and binaries
-$PYTHON -m pip install panda3d*.whl -vv
+$PREFIX/bin/python -m pip install panda3d*.whl -vv
 
 # Manual installation of other elements
 cd build
