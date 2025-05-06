3bd3ccff75ec331cbd93ce0a24c23e258edfab80
Olivier Roussel <olivier.roussel@inria.fr>
2025-05-06 18:16:26 +0200

    add RUST_BACKTRACE var env to trace rattler crash

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -20,6 +20,7 @@ export PYTHONUNBUFFERED=1
 export RECIPE_ROOT="${RECIPE_ROOT:-/home/conda/recipe_root}"
 export CI_SUPPORT="${FEEDSTOCK_ROOT}/.ci_support"
 export CONFIG_FILE="${CI_SUPPORT}/${CONFIG}.yaml"
+export RUST_BACKTRACE=1
 
 cat >~/.condarc <<CONDARC
 
