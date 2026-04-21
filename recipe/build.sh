d88fbc3ea452f25d46527fc477a65beacfbf3890
Olivier Roussel <olivier.roussel31@gmail.com>
2026-04-21 16:33:08 +0200

    free disk space by removing temp build dirs after install

--- recipe/build.sh
+++ recipe/build.sh
@@ -218,3 +218,10 @@ do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
     cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
 done
+
+# Remove build dir
+cd ..
+rm -rf build
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == 1 && "${CMAKE_CROSSCOMPILING_EMULATOR:-}" == "" ]]; then
+  rm -rf build_minimal
+fi
